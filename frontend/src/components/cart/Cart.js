731e9f8a6fbc828d9dff2a62941c4bb2a5a5e648
Beny9313 <beny.illyes@gmail.com>
2021-06-02 17:39:52 +0300

    Update Cart.js

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -71,7 +71,7 @@ const Cart = ({ history }) => {
                       </div>
 
                       <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                        <p id="card_item_price">{item.price}€</p>
+                        <p id="card_item_price">{item.price}RON</p>
                       </div>
 
                       <div className="col-4 col-lg-3 mt-4 mt-lg-0">
